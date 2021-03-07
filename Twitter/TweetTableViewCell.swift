44a937bf1b121aacd49f35b33b64fb3b54b52200
Adarsh Sushanth <adarsh_sushanth@yahoo.com>
2021-03-06 19:07:34 -0500

    twitter app

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Adarsh Sushanth on 3/6/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
